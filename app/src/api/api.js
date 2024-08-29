811ba14405ba32c8c91dcddff42b7f47684cdecf
Seungjae Lim <seungjaelim@kaist.ac.kr>
2024-08-29 15:13:42 +0900

    FIN

--- app/src/api/api.js
+++ app/src/api/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'https://39c1-182-226-43-93.ngrok-free.app/api/v1';
+const API_URL = 'https://cb16-2001-2d8-6a9c-1cd8-ad78-ae7b-fa31-75fd.ngrok-free.app/api/v1';
 
 const axiosInstance = axios.create({
   baseURL: API_URL,
